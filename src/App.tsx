e2b7bc37fda64a50ca08e500f89c9ae1d4ad3300
nkweku66 <nkweku66@gmail.com>
2024-06-28 20:17:49 +0000

    resolve merge conflict in the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-=======
 import { useState } from 'react'
 import GlobalStyle from './Styles/GlobalStyles'
 import Navbar from './components/navbar/Navbar'
 
->>>>>>> feature/navbar
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
-    <>   
-=======
     <>
       <GlobalStyle />
       <Navbar />
->>>>>>> feature/navbar
     </>
   )
 }
