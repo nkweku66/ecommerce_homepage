b9b2cde239e2795991b9f5a1b52d1112dcda8194
nkweku66 <nkweku66@gmail.com>
2024-06-28 11:44:06 +0000

    Add changes to src files

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from './Navbar.styles'
+
+const Navbar: React.FC = () => {
+  return (
+    <>
+        <S.Navmenu>
+            <S.Nav
+        </S.Navmenu>
+    </>
+  )
+}
+
+export default Navbar
