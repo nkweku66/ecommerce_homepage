395804bc2fc42b5a541f10bf52e1b5718fd8b101
nkweku66 <nkweku66@gmail.com>
2024-06-29 23:02:37 +0000

    Change the rows in the App styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
     display: grid;
-    grid-template-rows: 534px 266px;
+    grid-template-rows: 534px;
     grid-template-columns: 840px 600px;
 `
 
