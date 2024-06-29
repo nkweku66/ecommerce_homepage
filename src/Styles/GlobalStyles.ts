d0a40fd34601767eece30da0dd52b0652a4c9f82
nkweku66 <nkweku66@gmail.com>
2024-06-29 20:08:54 +0000

    Add desktop view for the hero section

--- src/Styles/GlobalStyles.ts
+++ src/Styles/GlobalStyles.ts
@@ -13,6 +13,9 @@ const GlobalStyle = createGlobalStyle`
 
         @media (1439px <= width <= 1440px) {
             width: 100%;
+            display: grid;
+            grid-template: 534px 266px / 420px 550px 440px;
+            border: 1px solid red;
         }
     }
 
